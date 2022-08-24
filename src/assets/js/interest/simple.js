f673d87f622fe79c53c2d9da9d9ea33b6fe210ad
RobertoBula <mozrobertocarlos@gmail.com>
2022-08-24 11:25:55 -0500

    interes simple

---
+++ src/assets/js/interest/simple.js
@@ -0,0 +1,8 @@
+$('#calculars').click(function () {
+    const datasimple = {
+        capitals: $('#capitals').val(),
+        interess: $('#interess').val(),
+        plazos: $('#plazos').val()
+    }
+    console.log(datasimple) 
+})
