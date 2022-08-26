a99f7a692cf9de685b03af3146d3e8efc335e05f
RobertoBula <mozrobertocarlos@gmail.com>
2022-08-26 17:02:25 -0500

    loque sea

--- src/assets/js/interest/compound.js
+++ src/assets/js/interest/compound.js
@@ -5,4 +5,10 @@ $('#calcular').click(function () {
         plazo: $('#plazo').val()
     }
     console.log(data) 
+
+    
+    const percent = data.interes/100
+    const result = eval("data.capital*(1+percent)**data.plazo")
+    
+    console.log(result)
 })
--- src/assets/js/interest/simple.js
+++ src/assets/js/interest/simple.js
@@ -1,8 +1,30 @@
-$('#calculars').click(function () {
+$('#calcular').click(function () {
     const datasimple = {
-        capitals: $('#capitals').val(),
-        interess: $('#interess').val(),
-        plazos: $('#plazos').val()
+        capital: $('#capital').val(),
+        interes: $('#interes').val(),
+        plazo: $('#plazo').val()
     }
     console.log(datasimple) 
+
+    // calcular el valor de la cuota
+    const percent =  datasimple.interes/100
+    const result =  eval("datasimple.capital*((1+percent)**datasimple.plazo*percent)/((1+percent)**datasimple.plazo-1)")
+   
+    console.log(result)
+
+    // calcula el interés
+
+    const interest = (datasimple.capital*percent*datasimple.plazo)
+    console.log(interest)
+
+    // calcula el valor total a pagar por el prestamo
+
+    const total = (parseInt(datasimple.capital)+parseInt(interest))
+    console.log(total)
+
+
+
+
 })
+
+
