63a6facca0efcfd2f6f0e04728b8bc990b7ff801
Centro de Innovacion <centroinnovacion@ESCEINN02.local>
2022-08-31 16:58:49 -0500

    fixed interest simple

--- src/assets/js/interest/simple.js
+++ src/assets/js/interest/simple.js
@@ -9,51 +9,42 @@ $("#calcular").click(function () {
 
   // calcular el valor de la cuota
 
-  const percent = datasimple.interes / 100
-  const convert = percent / 12 
+  const percent = datasimple.interes / 100;
+  const convert = percent / 12;
   const cuota = eval(
-    "datasimple.capital*(((1+percent)**datasimple.plazo)*percent)/(((1+percent)**datasimple.plazo)-1)"
+    "datasimple.capital*(((1+convert)**datasimple.plazo)*convert)/(((1+convert)**datasimple.plazo)-1)"
   );
   console.log(convert);
 
   console.log(cuota);
   // calcula el interés
 
-  let interest = datasimple.capital * percent * (datasimple.plazo / 12);
+  let interest = datasimple.capital * convert * (datasimple.plazo / 12);
 
- //console.log(interest);
+  //console.log(interest);
 
   // calcula el valor total a pagar por el prestamo
 
   const total = parseInt(datasimple.capital) + parseInt(interest);
   //console.log(total);
 
-  //   let acapital = cuota - interest;
-  //   let saldo = datasimple.capital - acapital;
+  let acapital = cuota - interest;
+  let saldo = datasimple.capital - acapital;
 
-  //   let tr = "";
-  //   for (let i = 0; i <= datasimple.plazo; i++) {
-  //     let abonoCapital = cuota - interest;
-  //     if (i !== 0) {
-  //       interest = saldo * percent * (datasimple.plazo/12);
-  //       saldo -= abonoCapital;
-  //       tr += `<tr>
-  //             <td>${i}</td>
-  //             <td>${cuota.toFixed(0)}</td>
-  //             <td>${interest.toFixed(0)}</td>
-  //             <td>${abonoCapital.toFixed(0)}</td>
-  //             <td>${saldo.toFixed(0)}</td>
-  //         </tr>`;
-  //     }else{
-  //         tr += `<tr>
-  //         <td>${i}</td>
-  //         <td>${0}</td>
-  //         <td>${0}</td>
-  //         <td>${0}</td>
-  //         <td>${saldo}</td>
-  //     </tr>`;
-  //     }
-  //   }
+  let tr = "";
+  for (let i = 1; i <= datasimple.plazo; i++) {
+    debugger;
+    let abonoCapital = cuota - interest;
+    interest = saldo * convert;
+      saldo -= abonoCapital;
+      tr += `<tr>
+              <td>${i}</td>
+              <td>${cuota.toFixed(0)}</td>
+              <td>${interest.toFixed(0)}</td>
+              <td>${abonoCapital.toFixed(0)}</td>
+              <td>${saldo.toFixed(0)}</td>
+          </tr>`;
+  }
 
-  //   $("#tbody").append(tr);
+  $("#tbody").append(tr);
 });
